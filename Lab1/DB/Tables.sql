2ab648bd957406c74a707a41347b0f7c5f43ccfd
JacobV <namelomax@gmail.com>
2025-03-19 15:15:11 +0800

    Visual fixes

--- Lab1/DB/Tables.sql
+++ Lab1/DB/Tables.sql
@@ -18,3 +18,31 @@ CREATE table OrderDishes(
     OrderId INT,
     FOREIGN KEY (OrderId) REFERENCES Orders(OrderId) ON DELETE CASCADE
 );
+
+-- Таблица для всех доступных блюд
+CREATE TABLE AvailableDishes (
+    AvailableDishID INT PRIMARY KEY IDENTITY(1,1), -- Уникальный ID с автоинкрементом
+    Name NVARCHAR(100) NOT NULL,                   -- Название блюда
+    Price int NOT NULL                  -- Цена блюда
+);
+
+-- Таблица для всех доступных опций
+CREATE TABLE AvailableOptions (
+    AvailableOptionID INT PRIMARY KEY IDENTITY(1,1), -- Уникальный ID с автоинкрементом
+    Name NVARCHAR(100) NOT NULL,                     -- Название опции
+    Price int NOT NULL                    -- Цена опции
+);
+
+SELECT 
+    d.Name AS DishName,
+    o.Name AS OptionName,
+    COUNT(*) AS OptionCount
+FROM 
+    OrderDishes od
+JOIN AvailableDishes d ON od.AvailableDishID = d.AvailableDishID
+JOIN OrderOptions oo ON oo.OrderDishID = od.OrderDishID
+JOIN AvailableOptions o ON oo.AvailableOptionID = o.AvailableOptionID
+GROUP BY 
+    d.Name, o.Name
+ORDER BY 
+    d.Name, OptionCount DESC;
